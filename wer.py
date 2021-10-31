391ce1764d19fb08cee6511430d1eba28c1e9404
Ketharani2000 <84461801+Ketharani2000@users.noreply.github.com>
2021-10-31 09:53:56 -0700

    Create wer.py

---
+++ wer.py
@@ -0,0 +1,16 @@
+num=int(input("Input number: "))
+count=0
+if num<2:
+    print("Invalid number")
+else:
+    for j in range(2,num+1):
+        proper_divisors=[]
+        total=0
+        for i in range(1,j):
+            if j%i==0:
+                proper_divisors.append(i)
+        for k in proper_divisors:
+            total+=k
+        if total>j:
+            count+=1
+print("Number of abundant numbers from 1 to",num,"is",count)            
